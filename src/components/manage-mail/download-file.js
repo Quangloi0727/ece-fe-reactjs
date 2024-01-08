bce90ad4d9e0b75043b195b46a9c4bb2c5364fa8
loinq <loinq@metechvn.com>
2024-01-08 15:34:24 +0700

    complete

--- src/components/manage-mail/download-file.js
+++ src/components/manage-mail/download-file.js
@@ -3,18 +3,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from '../buttons/buttons';
 import { DataService } from '../../config/dataService/dataService';
+import { openNotificationWithIcon } from '../notifications/notification';
 
 function DownLoadFile({ value }) {
   const downloadFile = async (idFile) => {
-    await DataService.downloadFile(`/email-attachment/${idFile}`).then((response) => {
-      const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', `${value?.attachment?.fileName}`);
-      document.body.appendChild(link);
-      link.click();
-    });
+    const response = await DataService.get(`/email-attachment/${idFile}`);
+    const urlResponse = response?.data?.data?.url;
+    const fileName = response?.data?.data?.fileName;
+    if (response.status !== 200 || !urlResponse) {
+      return openNotificationWithIcon('error', 'Thất bại !', 'Vui lòng kiểm tra lại !');
+    }
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', urlResponse, true);
+    xhr.responseType = 'blob';
+
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        const url = window.URL.createObjectURL(new Blob([xhr.response]));
+
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', fileName);
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      }
+    };
+
+    xhr.send();
   };
+
   return (
     <Button
       className="ant-button-filename truncate"
